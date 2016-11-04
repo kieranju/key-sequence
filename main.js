d35de29a99627613083680b06b790294dcc17703
Kieran Utz <Kieranju@gmail.com>
2016-11-04 12:30:50 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
-window.onload = function () {
+// example sequences
+var sequence1 = new KeySequence(
+    [38, 40, 38, 40],
+    function () { alert('up, down, up, down'); }
+);
+var sequence2 = new KeySequence(
+    [37, 39, 37, 39],
+    function () { alert('left, right, left, right'); }
+);
 
-    var sequence1 = new KeySequence(
-        [38, 40, 38, 40],
-        function () { alert('up, down, up, down'); }
-    );
-    var sequence2 = new KeySequence(
-        [37, 39, 37, 39],
-        function () { alert('left, right, left, right'); }
-    );
-
-    new KeySequenceHandler([sequence1, sequence2]);
-
-};
+// init
+new KeySequenceHandler([sequence1, sequence2]);
